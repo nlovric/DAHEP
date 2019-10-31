27a85f19e23a84f85aa9501a8052afd6cc2f8f78
nlovric <nlovric@pmfst.hr>
2019-10-31 17:26:34 +0100

    ispravke greske kod imenovanja Branchova za 2. cesticu

--- Vjezbe_4/Analyzer.cpp
+++ Vjezbe_4/Analyzer.cpp
@@ -47,14 +47,14 @@ void Analyzer::ConvertTxtToRootFile(string _inFileName){
 	tree->Branch("E1", &_product1_E, "_product1_E/F");
 	tree->Branch("pT1", &_product1_pT, "_product1_pT/F");
 
-	tree->Branch("n1", &_product2_Name, "_product2_Name/S");
-	tree->Branch("isB1", &_product2_isBoson, "_product2_isBoson/O");
-	tree->Branch("m1", &_product2_mass, "_product2_mass_product1_px/F");
-	tree->Branch("px1", &_product2_px, "_product2_px/F");
-	tree->Branch("py1", &_product2_py, "_product2_py/F");
-	tree->Branch("pz1", &_product2_pz, "_product2_pz/F");
-	tree->Branch("E1", &_product2_E, "_product2_E/F");
-	tree->Branch("pT1", &_product2_pT, "_product2_pT/F");
+	tree->Branch("n2", &_product2_Name, "_product2_Name/S");
+	tree->Branch("isB2", &_product2_isBoson, "_product2_isBoson/O");
+	tree->Branch("m2", &_product2_mass, "_product2_mass_product1_px/F");
+	tree->Branch("px2", &_product2_px, "_product2_px/F");
+	tree->Branch("py2", &_product2_py, "_product2_py/F");
+	tree->Branch("pz2", &_product2_pz, "_product2_pz/F");
+	tree->Branch("E2", &_product2_E, "_product2_E/F");
+	tree->Branch("pT2", &_product2_pT, "_product2_pT/F");
 
 	_inFile.open(_inFileName.c_str());
 
