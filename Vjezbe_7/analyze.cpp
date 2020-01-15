a7d904570b326487692a86ed76631c0ca5236f3a
nlovric <nlovric@pmfst.hr>
2020-01-15 11:21:22 +0100

    vjezbe 7

---
+++ Vjezbe_7/analyze.cpp
@@ -0,0 +1,16 @@
+#include <Analyzer.h>
+
+int main(){
+	// Declare Analyzer
+	Analyzer *analyzer = new Analyzer();
+
+	analyzer->PlotHistogram("/home/public/data/ggH125/ZZ4lAnalysis.root");
+ 	analyzer->PlotHistogram("/home/public/data/qqZZ/ZZ4lAnalysis.root");
+
+ 	analyzer->PlotMass();
+ 	analyzer->PlotDkin();
+ 	
+	delete analyzer;
+
+	return 0;
+}
